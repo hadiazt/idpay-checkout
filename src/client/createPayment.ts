1d4f18a9dc88780b09cc154ed0dd2be9c790fb0d
hadiazt <hadiazari30@gmail.com>
2024-01-21 03:19:20 +0330

    Update createPayment.ts

--- src/client/createPayment.ts
+++ src/client/createPayment.ts
@@ -1,21 +1,24 @@
-import { CreatePayment, Query } from "../types/CreatePayment";
+import { CreatePayment, Query } from "../types/CreatePaymentTypes";
 import { Config } from "../config";
 import { IDPay } from "./client";
+import { AxiosResponse } from "axios";
 
-export const createPayment = async function (
+export const createPayment = function (
   this: IDPay,
   input: CreatePayment
-) {
-  var Query: Query = {
-    name: input.Name ?? undefined,
-    mail: input.Mail ?? undefined,
-    phone: input.Phone ?? undefined,
-    amount: input.Amount,
-    order_id: input.Order_ID,
-    desc: input.Description ?? undefined,
-    callback: input.CallBackURL,
-  };
-  // console.log(this);
+): Promise<AxiosResponse> {
+  const { Name, Mail, Phone, Amount, Order_ID, Description, CallBackURL } =
+    input;
 
-  this._Axios("POST", Config.API.CreatePayment, Query);
+  const Query: Query = {
+    name: Name ?? undefined,
+    mail: Mail ?? undefined,
+    phone: Phone ?? undefined,
+    amount: Amount,
+    order_id: Order_ID,
+    desc: Description ?? undefined,
+    callback: CallBackURL,
+  };
+
+  return this._Axios("POST", Config.API.CreatePayment, Query);
 };
