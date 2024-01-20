fbdbd443dff7acbffc7bb57f984372df2983d6be
hadiazt <hadiazari30@gmail.com>
2024-01-21 03:14:12 +0330

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,10 @@ export const Config = {
     CreatePayment: "payment",
   },
   Doc: {
+    MAIN: "https://idpay.ir/web-service/v1.1",
+    ErrTable: "https://idpay.ir/web-service/v1.1/#ad39f18522",
+    PostMan:
+      "https://idpay.ir/web-service/v1.1/idpay-v1.1.postman_collection.json",
     KEY: "https://panel.idpay.ir/web-services",
   },
 };
