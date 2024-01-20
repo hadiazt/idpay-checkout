ff5c51e18fb26bc2ca44f0bed2bd2d100c0262df
hadiazt <hadiazari30@gmail.com>
2024-01-21 01:36:12 +0330

    add config

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const Config = {
+  URL: "https://api.idpay.ir/v1.1/",
+  API_KEYLength: 36,
+  API: {
+    CreatePayment: "payment",
+  },
+  Doc: {
+    KEY: "https://panel.idpay.ir/web-services",
+  },
+};
