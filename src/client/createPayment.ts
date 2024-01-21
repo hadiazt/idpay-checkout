18a1563092d26fa220d38ba83b9472313ec0888f
hadiazt <hadiazari30@gmail.com>
2024-01-21 15:35:11 +0330

    add property cheker

--- src/client/createPayment.ts
+++ src/client/createPayment.ts
@@ -1,24 +1,43 @@
-import { CreatePayment, Query } from "../types/CreatePaymentTypes";
+import {
+  CreatePaymentTypes,
+  Query,
+  _CreatePaymentTypes,
+  propertyLengthConstraints,
+} from "../types/CreatePaymentTypes";
 import { Config } from "../config";
 import { IDPay } from "./client";
 import { AxiosResponse } from "axios";
+import { validatePropertyLength } from "../functions/PropertyValidator";
 
 export const createPayment = function (
   this: IDPay,
-  input: CreatePayment
+  input: CreatePaymentTypes
 ): Promise<AxiosResponse> {
-  const { Name, Mail, Phone, Amount, Order_ID, Description, CallBackURL } =
-    input;
-
   const Query: Query = {
-    name: Name ?? undefined,
-    mail: Mail ?? undefined,
-    phone: Phone ?? undefined,
-    amount: Amount,
-    order_id: Order_ID,
-    desc: Description ?? undefined,
-    callback: CallBackURL,
+    name: input.Name ?? undefined,
+    mail: input.Mail ?? undefined,
+    phone: input.Phone ?? undefined,
+    amount: input.Amount,
+    order_id: input.Order_ID,
+    desc: input.Description ?? undefined,
+    callback: input.CallBackURL,
   };
+
+  for (const propertyName in propertyLengthConstraints) {
+    const constraints =
+      propertyLengthConstraints[propertyName as keyof CreatePaymentTypes];
+    const propertyValue = input[propertyName as keyof CreatePaymentTypes];
+    const propertyType = typeof propertyValue;
+    const PaymentType =
+      _CreatePaymentTypes[propertyName as keyof CreatePaymentTypes];
+    validatePropertyLength(
+      propertyValue,
+      constraints,
+      propertyName,
+      propertyType,
+      PaymentType
+    );
+  }
 
   return this._Axios("POST", Config.API.CreatePayment, Query);
 };
