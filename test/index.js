fbce53568112779a3b1b4179366df632a2199690
hadiazt <hadiazari30@gmail.com>
2024-01-21 15:15:47 +0330

    testing

--- test/index.js
+++ test/index.js
@@ -5,9 +5,10 @@ const Client = IDPayClient('6a7f99eb-7c20-4412-a972-6dfb7cd253a4', true);
 Client.CreatePayment({
   // Name?: String;
   // Mail?: String;
-  // Phone?: String;
-  // Amount: Number;
-  // Order_ID: 'String',
+  Phone: "String",
+  Amount: 1000,
+  Order_ID: 'Sing',
   // Description?: String;
-  // CallBackURL: String;
-}).then().catch((e) => console.log(e))
+  CallBackURL: "localhost:9000",
+})
+  .then().catch((e) => console.log(e))
