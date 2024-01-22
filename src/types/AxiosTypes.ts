4f081efa289ba29296fabd81165ca01fb6afc619
hadiazt <hadiazari30@gmail.com>
2024-01-23 00:19:48 +0330

    Update AxiosTypes.ts

--- src/types/AxiosTypes.ts
+++ src/types/AxiosTypes.ts
@@ -6,5 +6,19 @@ export type _AxiosErrorTypes = {
 export type _AxiosResponseTypes = {
   id: string;
   link: string;
-  status: number;
+  status: string;
+  track_id: string;
+  order_id: 101;
+  amount: number;
+  date: string;
+  payment: {
+    track_id: string;
+    amount: number;
+    card_no: string;
+    hashed_card_no: string;
+    date: string;
+  };
+  verify: {
+    date: string;
+  };
 };
