7273577066d2a93aea0444d23ba2b8f6e64cc782
hadiazt <hadiazari30@gmail.com>
2024-01-23 00:19:43 +0330

    Create VerifyPaymentTypes.ts

---
+++ src/types/VerifyPaymentTypes.ts
@@ -0,0 +1,14 @@
+export type VerifyPaymentTypes = {
+  ID: string;
+  Order_ID: string;
+};
+
+export const _VerifyPaymentTypes = {
+  ID: String,
+  Order_ID: String,
+};
+
+export type VerifyPaymentQuery = {
+  id: string;
+  order_id: string;
+};
