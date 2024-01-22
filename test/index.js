b5ce4d796e01ed4a79eacd6deb3779fa5d8027bc
hadiazt <hadiazari30@gmail.com>
2024-01-22 10:20:29 +0330

    Update index.js

--- test/index.js
+++ test/index.js
@@ -3,12 +3,12 @@ const { IDPayClient } = require('../lib/main.js')
 const Client = IDPayClient('6a7f99eb-7c20-4412-a972-6dfb7cd253a4', true);
 
 Client.CreatePayment({
-  // Name?: String;
-  // Mail?: String;
-  Phone: "String",
+  // Name: String,
+  // Mail: String,
+  Phone: "09036812503",
   Amount: 1000,
-  Order_ID: 'Sing',
-  // Description?: String;
+  Order_ID: 'Order_ID',
+  // Description: String;,
   CallBackURL: "localhost:9000",
 })
-  .then().catch((e) => console.log(e))
+  .then((r) => console.log(r)).catch((e) => console.log(e))
