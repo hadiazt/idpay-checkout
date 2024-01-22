962ac22f16fd62c6d2f801dafae8b4b3017b2b81
hadiazt <hadiazari30@gmail.com>
2024-01-23 01:05:50 +0330

    finish VerifyPayment

--- src/client/axiosHelper.ts
+++ src/client/axiosHelper.ts
@@ -1,12 +1,16 @@
 import { Config } from "../config";
 import { CreatePaymentQuery } from "../types/CreatePaymentTypes";
+import {
+  VerifyPaymentQuery,
+  VerifyPaymentStatusCodes,
+} from "../types/VerifyPaymentTypes";
 import { _AxiosErrorTypes, _AxiosResponseTypes } from "../types/AxiosTypes";
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 
 export const _Axios = function (
   Method: string,
   URL: string,
-  Data: CreatePaymentQuery
+  Data: CreatePaymentQuery | VerifyPaymentQuery
 ) {
   const config: AxiosRequestConfig = {
     method: Method,
@@ -19,13 +23,47 @@ export const _Axios = function (
     data: Data,
   };
 
-  return new Promise<{}>((resolve, reject) => {
+  return new Promise<any>((resolve, reject) => {
     axios(config)
       .then((res: AxiosResponse<_AxiosResponseTypes>) => {
-        const { id, link } = res.data;
+        const {
+          id,
+          link,
+          status,
+          track_id,
+          order_id,
+          amount,
+          date,
+          payment,
+          verify,
+        } = res.data;
+
         switch (URL) {
-          case "payment":
+          case Config.API.CreatePayment:
             resolve({ ID: id, Link: link });
+            break;
+          case Config.API.VerifyPayment:
+            const message = VerifyPaymentStatusCodes[status] || "N/A";
+
+            if (message !== "N/A") {
+              resolve(message);
+            } else {
+              resolve({
+                Status: status,
+                TrackID: track_id,
+                ID: id,
+                Order_ID: order_id,
+                Amount: amount,
+                Date: date,
+                Payment: {
+                  TrackID: payment.track_id,
+                  CardNo: payment.card_no,
+                  HashedCardNo: payment.hashed_card_no,
+                  Date: payment.date,
+                },
+                Verify: { Date: verify.date },
+              });
+            }
             break;
           default:
             reject("Unsupported URL");
