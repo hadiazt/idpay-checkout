4031f706967ac6b1bb3c89603cb9177b0c4e92fe
hadiazt <hadiazari30@gmail.com>
2024-01-23 00:14:30 +0330

    Refactor validation function.

--- src/functions/PropertyValidator.ts
+++ src/functions/PropertyValidator.ts
@@ -1,40 +1,36 @@
 import { Logger } from "./Logger";
 import { PropertyLengthConstraints } from "../types/CreatePaymentTypes";
 
-export const validatePropertyLength = (
+export const validateProperty = (
   value: string | number,
-  constraints: PropertyLengthConstraints,
   propertyName: string,
-  propertyType: string | number,
-  Type: StringConstructor | NumberConstructor
+  Type: string | number,
+  constraints?: PropertyLengthConstraints
 ): void => {
+  if ((!constraints && typeof value !== Type) || (value && typeof value !== Type)) {
+    Logger(`${propertyName} Has Invalid Entry Type\nIt Must Be ${Type}`);
+  }
+
+  if (!constraints) {
+    return;
+  }
+
   if (!value && !constraints.Optional) {
     Logger(`${propertyName} Is Required`);
   }
 
-  if (value) {
-    if (typeof value !== typeof Type()) {
-      Logger(
-        `${propertyName} Has Invalid Entry Type\nIt Must Be ${typeof Type()}`
-      );
-    }
+  if (typeof value === "number" && value < constraints.MinLength) {
+    Logger(
+      `${propertyName} Has Invalid Entry\nIt Must Be From ${constraints.MinLength} To ${constraints.MaxLength} Length`
+    );
+  }
 
-    if (typeof value === "number" && value < constraints.MinLength) {
-      Logger(
-        `${propertyName} Has Invalid Entry\nIt Must Be From ${constraints.MinLength} To ${constraints.MaxLength} Length`
-      );
-    }
-
-    if (
-      typeof value === "string" &&
-      (value.length > constraints.MaxLength ||
-        value.length < constraints.MinLength)
-    ) {
-      Logger(
-        `${propertyName} Has Invalid Entry\nIt Must Be From ${constraints.MinLength} To ${constraints.MaxLength} Length`
-      );
-    }
-
-    return;
+  if (
+    typeof value === "string" &&
+    (value.length > constraints.MaxLength || value.length < constraints.MinLength)
+  ) {
+    Logger(
+      `${propertyName} Has Invalid Entry\nIt Must Be From ${constraints.MinLength} To ${constraints.MaxLength} Length`
+    );
   }
 };
