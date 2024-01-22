948f339b6dbab95a5c6ea0f78083619c180010ab
hadiazt <hadiazari30@gmail.com>
2024-01-22 09:55:32 +0330

    finish validator

--- src/functions/PropertyValidator.ts
+++ src/functions/PropertyValidator.ts
@@ -8,24 +8,33 @@ export const validatePropertyLength = (
   propertyType: string | number,
   Type: StringConstructor | NumberConstructor
 ): void => {
-  if (!constraints.Optional && !value) {
+  if (!value && !constraints.Optional) {
     Logger(`${propertyName} Is Required`);
-  } else if (value) {
-    console.log(value);
-    
-    // console.log(expectedType);
-    // if (value.constructor !== expectedType) {
-    //   Logger(`${propertyName} Type Is Wrong\nIt Should Be ${typeof expectedType}`);
-    // }
-    // value = value.toString();
-    // console.log(value);
-    // if (
-    //   value.length > constraints.MaxLength ||
-    //   value.length < constraints.MinLength
-    // ) {
-    //   Logger(
-    //     `${propertyName} Has Invalid Entry\nIt Must Be From ${constraints.MinLength} To ${constraints.MaxLength} Length`
-    //   );
-    // }
+  }
+
+  if (value) {
+    if (typeof value !== typeof Type()) {
+      Logger(
+        `${propertyName} Has Invalid Entry Type\nIt Must Be ${typeof Type()}`
+      );
+    }
+
+    if (typeof value === "number" && value < constraints.MinLength) {
+      Logger(
+        `${propertyName} Has Invalid Entry\nIt Must Be From ${constraints.MinLength} To ${constraints.MaxLength} Length`
+      );
+    }
+
+    if (
+      typeof value === "string" &&
+      (value.length > constraints.MaxLength ||
+        value.length < constraints.MinLength)
+    ) {
+      Logger(
+        `${propertyName} Has Invalid Entry\nIt Must Be From ${constraints.MinLength} To ${constraints.MaxLength} Length`
+      );
+    }
+
+    return;
   }
 };
